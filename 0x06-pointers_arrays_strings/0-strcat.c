8918042201bf53e6263eac68744c765af2d5187a
alagahmed <ahmedalag833@gmail.com>
2023-09-21 05:27:40 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *_strcat - main function of the prototype
+  *
+  * @src: Concatenation parameter
+  *
+  * @dest: Concatenation parameter
+  *
+  * Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int f, p;
+	f = 0;
+	p = 0;
+	while (dest[f] != '\0')
+	{
+		f++;
+	}
+	while (src[p] != '\0')
+	{
+		dest[f] = src[p];
+		f++;
+		p++;
+	}
+	dest[f] = '\0';
+	return (dest);
+}
